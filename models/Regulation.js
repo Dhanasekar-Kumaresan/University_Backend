5dbb27900f97616d3f261ef3587f315eaa397005
Dhanasekar-Kumaresan <Dhanasekar.k@incture.com>
2021-05-05 22:33:33 +0530

    "Regulation Structure update"

--- models/Regulation.js
+++ models/Regulation.js
@@ -1,5 +1,40 @@
 const mongoose=require("mongoose")
 
+const Department_Details=new mongoose.Schema(
+  {
+    Department_ID:
+    {
+      type:String,
+      required:true,
+      trim:true,
+      unique:true
+    },
+    Department_Name:
+    {
+      type:String,
+      required:true,
+      trim:true
+    },
+  
+    total_Credit:
+    {
+      type:Number,
+      required:true
+    },
+    Semester_Count:
+    {
+      type:Number,
+      required:true
+    },
+    Credits_Details:
+    {
+      type:Array,
+      required:true
+  
+    }
+  }
+  );
+
 
 const Regulation_Schema=new mongoose.Schema(
 {
@@ -23,9 +58,14 @@ Academic_Year:
   type:Array,
   required:true,
 }
-
+,
+Department_Details:[Department_Details]
 
 }
-)
+);
+
+
+
 
 module.exports=mongoose.model("Regulation",Regulation_Schema);
+
