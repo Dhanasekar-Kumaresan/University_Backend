a17abbe009521c95e1a40936531686eb5db9e8f4
SreejaMadire <sreeja.madire@incture.com>
2021-06-03 16:16:18 +0530

    Revert "added update skeleton service"
    This reverts commit 77056fac381bafad55bbb26f74b63d1bffa21216.
    
    revert update skeleton commit

--- controllers/SubjectSkeletonsController.js
+++ controllers/SubjectSkeletonsController.js
@@ -1,84 +1,63 @@
 const SubjectSkeletons = require('../models/SubjectSkeletons')
 const Regulation = require("../models/Regulation");
-const { validationResult } = require("express-validator");
+const {validationResult}=require("express-validator");
 
-async function createSubjectSkeletons(req, res) {
-  var Errors = validationResult(req);
-  if (!Errors.isEmpty()) {
-    console.log("bad request");
-    return res.status(400).json({ errors: Errors.errors });
-  }
-  const subjectSkeletons = new SubjectSkeletons(req.body);
-  try {
-    await subjectSkeletons.save();
-    return res.status(201).json({ msg: "Success", data: subjectSkeletons });;
-  } catch (e) {
-    return res.status(400).json({ msg: e });
-  }
+async function createSubjectSkeletons(req,res){
+    var Errors=validationResult(req);
+    if (!Errors.isEmpty()) {
+        console.log("bad request");
+        return res.status(400).json({ errors: Errors.errors });
+      }
+      const  subjectSkeletons = new SubjectSkeletons(req.body);
+      try {
+        await subjectSkeletons.save();
+      return res.status(201).json({ msg: "Success" ,data:subjectSkeletons});;
+    } catch (e) {
+        return res.status(400).json({ msg: e });
+    }
 }
 
-async function getSubjectSkeletonByDefaults(res, res) {
+async function getSubjectSkeletonByDefaults(res,res){
 
-  try {
-    const skeletons = await SubjectSkeletons.find({});
-    return res.status(200).json({ msg: "Success", data: skeletons })
-  } catch (e) {
-    return res.status(400).json({ msg: e });
-  }
+    try{
+     const skeletons = await SubjectSkeletons.find({});
+     return res.status(200).json({ msg: "Success", data: skeletons })
+    }catch(e){
+        return res.status(400).json({ msg: e });
+    }
 
 }
 
-async function addSubjectSkeletonToInstitution(req, res) {
-  var institution = req.params.ins_id;
-  var regulation = req.params.reg_id;
-  try {
+async function addSubjectSkeletonToInstitution(req,res){
+    var institution=req.params.ins_id;
+    var regulation=req.params.reg_id;
+    try{
     await Regulation.updateOne(
-      {
-        Institution_id: institution,
-        Regulation: { $elemMatch: { Regulation_ID: regulation } }
-      },
-      {
+    {
+        Institution_id:institution,
+        Regulation:{ $elemMatch:{ Regulation_ID:regulation}}
+    },
+    {
         $push:
         {
-          "Regulation.$.evaluationCriteria": req.body
+          "Regulation.$.evaluationCriteria":req.body
         }
-      })
-    return res.status(200).json({ success: true });
-  }
-  catch (err) {
-    return res.status(400).json({
-      success: false,
-      message: error.message
-    });
-  }
-
-
-}
-
-async function updateSubjectSkeletons(req, res) {
-  var Errors = validationResult(req);
-  if (!Errors.isEmpty()) {
-    console.log("bad request");
-    return res.status(400).json({ errors: Errors.errors });
-  }
-
-  try {
-    const updatedSkeleton = await SubjectSkeletons.findOneAndUpdate({ subject_type: req.params.subject_type }, req.body,{
-      new: true,
-      upsert: true // Make this update into an upsert
     })
-    return res.status(201).json({ msg: "Subject skeleton updated successfully", data: updatedSkeleton });;
+    return res.status(200).json({ success: true });}
+    catch(err){
+        return res.status(400).json({
+          success: false,
+          message: error.message
+        });
+      }
 
-  } catch (e) {
-    return res.status(400).json({ msg: e });
-
-  }
+    
 }
 
 
-module.exports = {
-  createSubjectSkeletons,
-  getSubjectSkeletonByDefaults,
-  addSubjectSkeletonToInstitution,
-  updateSubjectSkeletons
+
+module.exports={
+    createSubjectSkeletons,
+    getSubjectSkeletonByDefaults,
+    addSubjectSkeletonToInstitution
 }
